12ece4dab2414bf6b38350cf7dbd9cb873b4c6c9
Alvaro Chavez <a.chavezs@duocuc.cl>
2025-11-06 20:37:11 -0300

    CONFIG: CAMBIOS .JSON PRODUCTOS, APP Y MAIN

--- milsabores-app/src/App.jsx
+++ milsabores-app/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom'
+import { HashRouter, Route, Routes } from 'react-router-dom'
 import Inicio from './pages/Inicio.jsx'
 import Blog from './pages/Blog.jsx'
 import Carrito from './pages/Carrito.jsx'
@@ -16,19 +16,19 @@ function App() {
   return (
     <>
       <MensajeFlotante />
-      <Routes>
-        <Route path="/" element={<Inicio />} />
-        <Route path="/inicio" element={<Inicio />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/carrito" element={<Carrito />} />
-        <Route path="/catalogo" element={<Catalogo />} />
-        <Route path="/producto/:id" element={<Producto />} />
-        <Route path="/contacto" element={<Contacto />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/quienes_somos" element={<QuienesSomos />} />
-        <Route path="/registro_user" element={<RegistroUser />} />
-        <Route path="/admin_dashboard" element={<AdminDashboard />} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Inicio />} />
+          <Route path="/inicio" element={<Inicio />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/carrito" element={<Carrito />} />
+          <Route path="/catalogo" element={<Catalogo />} />
+          <Route path="/producto/:id" element={<Producto />} />
+          <Route path="/contacto" element={<Contacto />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/quienes_somos" element={<QuienesSomos />} />
+          <Route path="/registro_user" element={<RegistroUser />} />
+          <Route path="/admin_dashboard" element={<AdminDashboard />} />
+        </Routes>
     </>  
   )
 }
--- milsabores-app/src/main.jsx
+++ milsabores-app/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { CarritoProvider } from './context/CarritoContext';
 import './index.css'
 import App from './App.jsx'
@@ -9,9 +9,9 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <CarritoProvider>    
-      <BrowserRouter>
+      <HashRouter>
           <App />
-      </BrowserRouter>
+      </HashRouter>
     </CarritoProvider>  
   </StrictMode>,
 )
